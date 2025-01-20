a1d2e9efdf6fce3eb5b5238e81240fb63c9db1b1
Satviky <126392608+Satviky@users.noreply.github.com>
2025-01-20 19:09:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const path = require('path');
 const MongooseConnect = require('./DBconnect.js');
 const { User } = require('./models/userModel.js');
 
 // Connect to MongoDB
 MongooseConnect();
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.get('/', (req, res) => {
     res.render('index')
