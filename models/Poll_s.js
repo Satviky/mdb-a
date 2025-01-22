e95a979a76d572ce20ce2a5ea738f586d7aa559a
Satviky <126392608+Satviky@users.noreply.github.com>
2025-01-22 11:22:03 +0530

    Update Poll_s.js

--- models/Poll_s.js
+++ models/Poll_s.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const AutoIncrement = require('mongoose-sequence')(mongoose);
 const UserSchema = new mongoose.Schema({
-    title: {type: String, required: true, default: "New Vote Jan 21"},
+    title: {type: String, required: true, default: "New Vote Jan 22 @ half past 11"},
     desc: String,
     isDone: Boolean,
     vc: { type: Number, unique: true }
